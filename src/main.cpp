fa86db840e4292315aeb8d48567815423bb75ed6
MalikHw <help.malicorporation@gmail.com>
2026-06-26 19:30:15 +0100

    A

--- src/main.cpp
+++ src/main.cpp
@@ -3,58 +3,76 @@
 
 using namespace geode::prelude;
 
+// yes i had this prob so why not make it to a mod
+class DragHandleItem : public CCMenuItemSpriteExtra {
+public:
+    ColorSelectLiveOverlay* m_targetLayer;
+    CCPoint m_startTouchPos;
+    CCPoint m_startLayerPos;
+    bool m_isDragging;
+
+    static DragHandleItem* create(CCNode* normalSprite, ColorSelectLiveOverlay* target) {
+        auto ret = new DragHandleItem();
+        if (ret && ret->init(normalSprite, nullptr, nullptr)) {
+            ret->m_targetLayer = target;
+            ret->m_isDragging = false;
+            ret->autorelease();
+            return ret;
+        }
+        CC_SAFE_DELETE(ret);
+        return nullptr;
+    }
+
+    virtual bool ccTouchBegan(CCTouch* touch, CCEvent* event) {
+        if (!m_bEnabled || !m_bVisible) return false;
+        
+        CCPoint touchPos = this->getParent()->convertTouchToNodeSpace(touch);
+        if (this->boundingBox().containsPoint(touchPos)) {
+            m_startTouchPos = m_targetLayer->convertTouchToNodeSpace(touch);
+            m_startLayerPos = m_targetLayer->getPosition();
+            m_isDragging = true;
+            return true;
+        }
+        return CCMenuItemSpriteExtra::ccTouchBegan(touch, event);
+    }
+
+    virtual void ccTouchMoved(CCTouch* touch, CCEvent* event) {
+        if (m_isDragging) {
+            CCPoint touchPos = m_targetLayer->convertTouchToNodeSpace(touch);
+            CCPoint delta = ccpSub(touchPos, m_startTouchPos);
+            m_targetLayer->setPosition(ccpAdd(m_startLayerPos, delta));
+        } else {
+            CCMenuItemSpriteExtra::ccTouchMoved(touch, event);
+        }
+    }
+
+    virtual void ccTouchEnded(CCTouch* touch, CCEvent* event) {
+        m_isDragging = false;
+        CCMenuItemSpriteExtra::ccTouchEnded(touch, event);
+    }
+
+    virtual void ccTouchCancelled(CCTouch* touch, CCEvent* event) {
+        m_isDragging = false;
+        CCMenuItemSpriteExtra::ccTouchCancelled(touch, event);
+    }
+};
+
 class $modify(CSLO, ColorSelectLiveOverlay) {
     struct Fields {
-        CCSprite* m_dragButton;
-        CCPoint m_dragStartPos;
-        CCPoint m_layerStartPos;
-        bool m_isDragging;
+        CCMenu* m_dragMenu;
     };
 
     bool init(ColorAction* baseAction, ColorAction* detailAction, EffectGameObject* object) {
         if (!ColorSelectLiveOverlay::init(baseAction, detailAction, object)) return false;
         
-        m_fields->m_dragButton = CCSprite::create("GJ_colorThumbSBtn.png");
-        if (m_fields->m_dragButton) {
-            m_fields->m_dragButton->setPosition(ccp(220, 170));
-            this->addChild(m_fields->m_dragButton, 10);
-        }
-
-        m_fields->m_isDragging = false;
+        m_fields->m_dragMenu = CCMenu::create();
+        m_fields->m_dragMenu->setPosition(ccp(0, 0));
+        this->addChild(m_fields->m_dragMenu, 10);
+        auto dragBtn = CCSprite::create("GJ_colorThumbSBtn.png");
+        auto dragItem = DragHandleItem::create(dragBtn, this);
+        dragItem->setPosition(ccp(220, 170));
+        m_fields->m_dragMenu->addChild(dragItem);
 
         return true;
-    }
-
-    bool ccTouchBegan(CCTouch* touch, CCEvent* event) {
-        if (!m_fields->m_dragButton) return false;
-        
-        CCPoint touchPos = this->convertTouchToNodeSpace(touch);
-        if (m_fields->m_dragButton->boundingBox().containsPoint(touchPos)) {
-            m_fields->m_dragStartPos = touchPos;
-            m_fields->m_layerStartPos = this->getPosition();
-            m_fields->m_isDragging = true;
-            return true;
-        }
-        return false;
-    }
-
-    void ccTouchMoved(CCTouch* touch, CCEvent* event) {
-        if (!m_fields->m_isDragging) return;
-        
-        CCPoint touchPos = this->convertTouchToNodeSpace(touch);
-        CCPoint delta = ccpSub(touchPos, m_fields->m_dragStartPos);
-        this->setPosition(ccpAdd(m_fields->m_layerStartPos, delta));
-    }
-
-    void ccTouchEnded(CCTouch* touch, CCEvent* event) {
-        m_fields->m_isDragging = false;
-    }
-
-    void ccTouchCancelled(CCTouch* touch, CCEvent* event) {
-        m_fields->m_isDragging = false;
-    }
-
-    void registerWithTouchDispatcher() {
-        CCDirector::sharedDirector()->getTouchDispatcher()->addTargetedDelegate(this, -128, true);
     }
 };
