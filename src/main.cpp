b4ee855d2113ce395dde98fd1544b2106a13ba12
MalikHw <help.malicorporation@gmail.com>
2026-06-26 21:12:39 +0100

    fdghgfd

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,14 @@
 using namespace geode::prelude;
 
 // yes i had this prob so why not make it to a mod
-class DragHandleItem : public CCLayer {
+class DragHandleItem : public CCMenuItemSpriteExtra {
 public:
     CCNode* m_targetLayer;
-    CCSprite* m_sprite;
     CCPoint m_startTouchPos;
     CCPoint m_startLayerPos;
     bool m_isDragging;
 
-    static DragHandleItem* create(CCSprite* sprite, CCNode* target) {
+    static DragHandleItem* create(const char* sprite, CCNode* target) {
         auto ret = new DragHandleItem();
         if (ret && ret->init(sprite, target)) {
             ret->autorelease();
@@ -23,28 +22,19 @@ public:
         return nullptr;
     }
 
-    bool init(CCSprite* sprite, CCNode* target) {
-        if (!CCLayer::init()) return false;
-        m_sprite = sprite;
+    bool init(const char* sprite, CCNode* target) {
+        auto spr = CCSprite::create(sprite);
+        if (!CCMenuItemSpriteExtra::init(spr, spr, nullptr, nullptr)) return false;
         m_targetLayer = target;
         m_isDragging = false;
-        this->setContentSize(sprite->getContentSize());
-        this->addChild(sprite);
-        sprite->setPosition(m_obContentSize / 2);
-        this->setTouchEnabled(true);
-        this->setTouchPriority(-128);
         return true;
     }
 
-    virtual void registerWithTouchDispatcher() {
-        CCDirector::sharedDirector()->getTouchDispatcher()->addTargetedDelegate(this, this->getTouchPriority(), true);
-    }
-
     virtual bool ccTouchBegan(CCTouch* touch, CCEvent* event) {
-        if (!m_bVisible || !m_targetLayer) return false;
+        if (!m_bEnabled || !m_bVisible || !m_targetLayer) return false;
         
-        CCPoint touchPos = this->convertTouchToNodeSpace(touch);
-        if (CCRect(CCPointZero, this->getContentSize()).containsPoint(touchPos)) {
+        CCPoint touchPos = this->getParent()->convertTouchToNodeSpace(touch);
+        if (this->boundingBox().containsPoint(touchPos)) {
             m_startTouchPos = m_targetLayer->convertTouchToNodeSpace(touch);
             m_startLayerPos = m_targetLayer->getPosition();
             m_isDragging = true;
@@ -57,7 +47,15 @@ public:
         if (m_isDragging) {
             CCPoint touchPos = m_targetLayer->convertTouchToNodeSpace(touch);
             CCPoint delta = ccpSub(touchPos, m_startTouchPos);
-            m_targetLayer->setPosition(ccpAdd(m_startLayerPos, delta));
+            CCPoint newPos = ccpAdd(m_startLayerPos, delta);
+
+            CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+            CCSize overlaySize = m_targetLayer->getContentSize();
+            
+            newPos.x = clampf(newPos.x, 0, winSize.width - overlaySize.width);
+            newPos.y = clampf(newPos.y, 0, winSize.height - overlaySize.height);
+            
+            m_targetLayer->setPosition(newPos);
         }
     }
 
@@ -72,17 +70,21 @@ public:
 
 class $modify(CSLO, ColorSelectLiveOverlay) {
     struct Fields {
+        CCMenu* m_dragMenu;
         DragHandleItem* m_dragItem;
     };
 
     bool init(ColorAction* baseAction, ColorAction* detailAction, EffectGameObject* object) {
         if (!ColorSelectLiveOverlay::init(baseAction, detailAction, object)) return false;
         
-        auto dragBtn = CCSprite::create("GJ_colorThumbSBtn.png");
-        auto dragItem = DragHandleItem::create(dragBtn, this);
-        dragItem->setPosition(ccp(220, 170));
-        this->addChild(dragItem, 10);
-        m_fields->m_dragItem = dragItem;
+        m_fields->m_dragMenu = CCMenu::create();
+        m_fields->m_dragMenu->setPosition(ccp(0, 0));
+        m_fields->m_dragMenu->setTouchEnabled(true);
+        m_fields->m_dragMenu->setTouchPriority(-128);
+        this->addChild(m_fields->m_dragMenu, 10);
+        m_fields->m_dragItem = DragHandleItem::create("GJ_colorThumbSBtn.png", this);
+        m_fields->m_dragItem->setPosition(ccp(220, 170));
+        m_fields->m_dragMenu->addChild(m_fields->m_dragItem);
 
         return true;
     }
@@ -90,17 +92,21 @@ class $modify(CSLO, ColorSelectLiveOverlay) {
 
 class $modify(HLO, HSVLiveOverlay) {
     struct Fields {
+        CCMenu* m_dragMenu;
         DragHandleItem* m_dragItem;
     };
 
     bool init(GameObject* object, cocos2d::CCArray* objects) {
         if (!HSVLiveOverlay::init(object, objects)) return false;
         
-        auto dragBtn = CCSprite::create("GJ_colorThumbSBtn.png");
-        auto dragItem = DragHandleItem::create(dragBtn, this);
-        dragItem->setPosition(ccp(220, 170));
-        this->addChild(dragItem, 10);
-        m_fields->m_dragItem = dragItem;
+        m_fields->m_dragMenu = CCMenu::create();
+        m_fields->m_dragMenu->setPosition(ccp(0, 0));
+        m_fields->m_dragMenu->setTouchEnabled(true);
+        m_fields->m_dragMenu->setTouchPriority(-128);
+        this->addChild(m_fields->m_dragMenu, 10);
+        m_fields->m_dragItem = DragHandleItem::create("GJ_colorThumbSBtn.png", this);
+        m_fields->m_dragItem->setPosition(ccp(220, 170));
+        m_fields->m_dragMenu->addChild(m_fields->m_dragItem);
 
         return true;
     }
